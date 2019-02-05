2c4c8d24043ff5abd54c7dda0bc4d59e7847c078
amypetrie <apetrie9849@gmail.com>
2019-02-05 10:03:43 -0700

    Write function to call API returning object (WIP)

--- docs/bundle.js
+++ docs/bundle.js
@@ -80,6 +80,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
 
+const button = document.getElementbyId("breakdownButton");
 
 __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(() => {
   // have fun!
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import $ from 'jquery'
 
+const getTopWord = () => {
+  $('#topWord').html('');
+
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+    .then(response => response.json())
+    .then(word_returned => setTopWord(word_returned))
+    .catch(error => console.error({ error }));
+};
+
+function setTopWord(top_word){
+  let word = top_word.word;
+  $("#topWord").append(`${word}`);
+}
+
 $(document).ready(() => {
-  // have fun!
+  $( window ).on( "load", getTopWord );
 })
